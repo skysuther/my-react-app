31badfe7710eca1b9fa725c4d551aad327966ed4
skysuther <skysuther@users.noreply.github.com>
2022-07-19 23:43:07 -0400

    Nav Bar

---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+import './Nav.css';
+
+function Nav() {
+  return (
+    <div className="Nav">
+      <header className="Nav-header">
+        <ul className="Nav-list">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default Nav;
