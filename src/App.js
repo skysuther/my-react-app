bac89b7ecfdc59bf65fdb15d6e38003b40e5983c
skysuther <skysuther@users.noreply.github.com>
2022-07-19 20:26:34 -0400

    Editing page

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ function App() {
         <p>
           Welcome to Skyler's new app!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
