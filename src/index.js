771ac8a92dd32b1e709720b80ec56ef0f2a3329a
skysuther <skysuther@users.noreply.github.com>
2022-07-19 23:54:55 -0400

    p

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Nav from './Nav';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Nav />
     <App />
   </React.StrictMode>
 );
